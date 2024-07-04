4ce210f610dd6b2cca33f9db1f8dbbeeace48927
WillRob <william.roberge.wr@gmail.com>
2024-07-04 18:42:08 -0400

    add https

--- app/src/app/routes.py
+++ app/src/app/routes.py
@@ -1,4 +1,5 @@
 from flask import current_app as app, render_template
+from flask import Flask, send_file
 
 @app.route('/')
 def home():
@@ -7,3 +8,9 @@ def home():
 @app.route('/carte')
 def carte():
     return render_template('cartevierge.html')
+
+@app.route('/logo')
+def logo():
+    # Assurez-vous que l'image est dans le répertoire "static/images/"
+    image_path = '..\..\\rst\image\logo\image.png'
+    return send_file(image_path, mimetype='image/png')
